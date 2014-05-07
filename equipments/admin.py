2b91a221ca27c0723a130807b4d04b40c167d7d3
isaacrdz <zack_cpp@hotmail.com>
2014-05-07 17:26:10 -0500

    ready

--- equipments/admin.py
+++ equipments/admin.py
@@ -6,7 +6,7 @@ class EquipmentAdmin(admin.ModelAdmin):
 	list_display = ('brand','family','model','specification')
 	list_filter =  ('brand','model')
 	search_fields = ( )
-	list_editable = ('family','model',)
+	list_editable = ('family','model', )
 	raw_id_fields = ('brand',)
 	actions = (export_as_excel,)
 
